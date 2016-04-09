29038604ccf5df5750b99c6caae065368e63b929
yuxin <cyx32524@gmail.com>
2016-04-09 15:05:19 -0400

    add test on GPIO

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import re
 import urllib2
 import cStringIO
 import base64
-import FirebaseConnector
+# import FirebaseConnector
 
 firebase_url = 'https://event-finder-test.firebaseio.com'
 
@@ -34,23 +34,23 @@ if __name__ == '__main__':
 	pygame.time.set_timer(USEREVENT+2, 5000)  #5 seconds 
 
 	events_arr = []
-	FirebaseConnector.packEventsToEventsData(events_arr, firebase_url)
+#	FirebaseConnector.packEventsToEventsData(events_arr, firebase_url)
 
-	cur_event_idx = 0
-	cur_img_idx = 0
+#	cur_event_idx = 0
+#	cur_img_idx = 0
 
-	cur_events_images_arr = events_arr[cur_event_idx].getImageArray()
-	img_str = cur_events_images_arr[cur_img_idx]
+#	cur_events_images_arr = events_arr[cur_event_idx].getImageArray()
+#	img_str = cur_events_images_arr[cur_img_idx]
 	# print img_str
 	# f = open('workfile', 'w')
 	# f.write(img_str)
 
 	# print type(img_str)
 
-	img_str = "https://d3e1o4bcbhmj8g.cloudfront.net/photos/357497/big/0e2feb711243eca3cdbcb830029d6198f2b0212e.jpg"
+#	img_str = "https://d3e1o4bcbhmj8g.cloudfront.net/photos/357497/big/0e2feb711243eca3cdbcb830029d6198f2b0212e.jpg"
 
-	img_str = "https://stackoverflow.com"
-	# img_str = "http://calligraphyalphabet.org/1234/wp-content/uploads/brush-calligraphy-alphabet-l.jpg"
+#	img_str = "https://stackoverflow.com"
+	img_str = "http://calligraphyalphabet.org/1234/wp-content/uploads/brush-calligraphy-alphabet-l.jpg"
 
 	while True:
 
